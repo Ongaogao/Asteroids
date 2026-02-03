05fd04e11f7edcc15948df8964bd1aefe5975335
Ongaogao <seb.zeine@gmail.com>
2026-02-03 11:15:21 +0100

    black screen fixes

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ from constants import *
 from logger import log_state
 
 
+
 def main():
-    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
-    infinite_loop()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    infinite_loop(screen)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
 
-def infinite_loop():
+
+def infinite_loop(screen):
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
             
