ada7db00b359d5b123c9289db73b7e75e4b40fa9
Ongaogao <seb.zeine@gmail.com>
2026-02-07 22:38:13 +0100

    Create group

--- main.py
+++ main.py
@@ -10,16 +10,20 @@ def main():
     pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    infinite_loop(screen, clock, frame_rate=60, player = player)
-    
+    infinite_loop(screen, clock, frame_rate=60, player = player, updatable=updatable, drawable=drawable)
 
     
 
+    
 
-def infinite_loop(screen, clock, frame_rate, player):
+
+def infinite_loop(screen, clock, frame_rate, player, updatable, drawable):
     dt = 0
     while True:
         log_state()
@@ -28,9 +32,10 @@ def infinite_loop(screen, clock, frame_rate, player):
                 return
 
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         
         pygame.display.flip()
         dt = clock.tick(frame_rate) / 1000.0
